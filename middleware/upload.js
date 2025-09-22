98d3ef1efcfb0b960cc9e93d7e8943e8d8b6d576
Rahiman13 <rahimanshaik13@gmail.com>
2025-09-22 15:53:32 +0530

    s3 preview

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,3 +1,26 @@
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
+// const s3 = require('../config/s3');
+
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: process.env.AWS_BUCKET_NAME,
+//         // Remove the ACL line
+//         metadata: (req, file, cb) => {
+//             cb(null, { fieldName: file.fieldname });
+//         },
+//         key: (req, file, cb) => {
+//             const fileName = `uploads/${Date.now()}-${file.originalname}`;
+//             cb(null, fileName);
+//         },
+//     }),
+// });
+
+// module.exports = upload;
+
+
+
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = require('../config/s3');
@@ -6,7 +29,7 @@ const upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: process.env.AWS_BUCKET_NAME,
-        // Remove the ACL line
+        contentType: multerS3.AUTO_CONTENT_TYPE, // ✅ fixes download issue
         metadata: (req, file, cb) => {
             cb(null, { fieldName: file.fieldname });
         },
